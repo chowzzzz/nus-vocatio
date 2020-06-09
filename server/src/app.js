7fa1a3d4150ce72bd75799bff8e0b0468b8f6b85
Mok XF <mokxf16@gmail.com>
2020-06-09 12:06:39 +0800

    add new service to client

--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,15 @@ app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/status", (req, res) => {
+/* app.get("/status", (req, res) => {
     res.send({
         message: "hello world!"
+    });
+}); */
+
+app.post("/register", (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! You are now registered, have fun!`
     });
 });
 
