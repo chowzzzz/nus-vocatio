ace4d53b3d905533e8b590ca882776ef411c6e20
Mok XF <mokxf16@gmail.com>
2020-06-19 11:54:17 +0800

    edit responsive css of filter menu

--- client/src/components/SideFilterMenu.vue
+++ client/src/components/SideFilterMenu.vue
@@ -17,7 +17,7 @@
         </div>
         <div class="filter-block">
             <h4 class="filter-content-title">
-                Type
+                Types
                 <img src="../assets/down.svg" alt="Down" class="down" />
             </h4>
             <ul class="filter-content">
@@ -179,5 +179,11 @@ li {
 
 #salarySlider {
     width: 90%;
+}
+
+@media screen and (max-width: 900px) {
+    .side-filter-menu {
+        width: 25%;
+    }
 }
 </style>
