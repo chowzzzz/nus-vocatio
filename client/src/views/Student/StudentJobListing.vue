f663987a0319a946855590fd68a5fda31472cb9d
Mok XF <mokxf16@gmail.com>
2020-06-26 15:20:32 +0800

    change back btn to be component

--- client/src/views/Student/StudentJobListing.vue
+++ client/src/views/Student/StudentJobListing.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <button id="backBtn" @click="$router.go(-1)">
-            <img src="../../assets/left.svg" alt="back button" /> Back to jobs
-        </button>
+        <backBtn title="jobs"></backBtn>
         <div class="joblist-container">
             <jobListing />
             <employerContact user="student" />
@@ -11,12 +9,14 @@
 </template>
 
 <script>
+import BackBtn from "../../components/BackBtn.vue";
 import JobListing from "../../components/JobListing.vue";
 import EmployerContact from "../../components/EmployerContactSideMenu.vue";
 
 export default {
     name: "StudentJobListing",
     components: {
+        backBtn: BackBtn,
         jobListing: JobListing,
         employerContact: EmployerContact
     }
@@ -25,29 +25,6 @@ export default {
 
 
 <style scoped>
-#backBtn {
-    margin: 2em 3em 0;
-    border: none;
-    padding: 0.5em 1em;
-    border-radius: 25px;
-    outline: none;
-    background: #d0e1eb;
-    color: #2b839e;
-    transition-duration: 0.4s;
-    cursor: pointer;
-}
-
-#backBtn:hover {
-    background-color: #47a1bd;
-    color: #fff;
-}
-
-#backBtn img {
-    padding-top: 3px;
-    width: 10px;
-    border: none;
-}
-
 .joblist-container {
     display: grid;
     grid-template-columns: 70% auto;
@@ -78,10 +55,6 @@ export default {
 }
 
 @media screen and (max-width: 570px) {
-    #backBtn {
-        display: none;
-    }
-
     .joblist-container {
         margin: 1.5em 2em;
     }
