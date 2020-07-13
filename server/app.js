13c956adbca1f3f8658754d15ca02b048e15c324
Mok XF <mokxf16@gmail.com>
2020-07-13 16:28:41 +0800

    change port to 8081

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+    origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
@@ -22,7 +22,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to vocatio application." });
+    res.json({ message: "Welcome to vocatio application." });
 });
 
 require("./routes/student.routes")(app);
@@ -32,9 +32,8 @@ require("./routes/employer.routes")(app);
 require("./routes/jobpost.routes")(app);
 require("./routes/application.routes")(app);
 
-
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
