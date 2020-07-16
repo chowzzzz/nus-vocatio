16999e2d9a8b89cbda123c47a9cfeeb87ff88c33
Levron Goh <gohlevron@gmail.com>
2020-07-16 17:43:14 +0800

    Create main.native.js

---
+++ client/src/main.native.js
@@ -0,0 +1,37 @@
+import Vue from 'nativescript-vue';
+import App from './App.vue';
+import router from "./router";
+import store from "./store";
+// import { sync } from "vuex-router-sync";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import moment from "moment";
+import VueSwal from "vue-swal";
+
+Vue.config.silent = false;
+Vue.config.productionTip = false;
+
+// format date using moment
+Vue.filter("formatDate", function(value) {
+    if (value) {
+        return moment(String(value)).format("DD MMM YYYY");
+    }
+});
+
+// sync(store, router);
+
+// vue sweet alert
+Vue.use(VueSwal);
+
+new Vue({
+    router,
+    store,
+    render: h => h('frame', [h(App)]),
+  }).$start();
+/*
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App)
+}).$mount("#app");
+*/
