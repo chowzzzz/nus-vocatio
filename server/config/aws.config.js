4721ac01b151139c333150f20ee436735e92bf11
Mok XF <38778609+chowzzzz@users.noreply.github.com>
2021-03-22 12:07:42 +0800

    Delete aws.config.js

--- server/config/aws.config.js
+++
@@ -1,8 +0,0 @@
-const aws = require("aws-sdk");
-
-aws.config.update({
-	accessKeyId: "AKIAWSZQYPQZC4XQ4UFX",
-	secretAccessKey: "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"
-});
-
-module.exports = aws;
